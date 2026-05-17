6caa5f574019151f343260d698f941ae8e0c7bee
NaclTea <53270339+NaclTea@users.noreply.github.com>
2026-05-18 00:04:52 +0100

    fix: add exit 0 and cleanup to install-singularity.sh bootstrap script

--- cluster/install-singularity.sh
+++ cluster/install-singularity.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Install Singularity on cluster nodes
+set -e # Fail on any error
 
-wget https://github.com/sylabs/singularity/releases/download/v3.11.4/singularity-ce-3.11.4-1.el7.x86_64.rpm
-sudo yum install -y singularity-ce-3.11.4-1.el7.x86_64.rpm
+# Install Singularity CE on Amazon Linux 2
+SINGULARITY_VERSION="3.11.4"
 
-ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-BUCKET_NAME="openfoam-cluster-bootstrap-${ACCOUNT_ID}"
+wget https://github.com/sylabs/singularity/releases/download/v${SINGULARITY_VERSION}/singularity-ce-${SINGULARITY_VERSION}-1.el7.x86_64.rpm
+sudo yum install -y singularity-ce-${SINGULARITY_VERSION}-1.el7.x86_64.rpm
+rm -f singularity-ce-${SINGULARITY_VERSION}-1.el7.x86_64.rpm
 
-aws s3 mb s3://${BUCKET_NAME} --region eu-west-2
-aws s3 cp ~/hpc-openfoam/cluster/install-singularity.sh s3://${BUCKET_NAME}/
+# Verify installation
+singularity --version
