e8e7f6987e3535aed56a2e8a837ffbad077a185e
NaclTea <53270339+NaclTea@users.noreply.github.com>
2026-05-18 13:49:36 +0100

    adding scripot to start mpi paraview on cluster

---
+++ paraview/pvserver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=pvserver
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=compute
+#SBATCH --output=/shared/paraview-server.log
+#SBATCH --time=01:00:00
+
+mpirun -np 32 /shared/paraview/bin/pvserver \
+    --mpi \
+    --server-port=11111
