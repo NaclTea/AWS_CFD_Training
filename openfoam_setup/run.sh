dc95654b21648b488ffd5a878c91b9ac6a18f8d6
NaclTea <53270339+NaclTea@users.noreply.github.com>
2026-05-18 01:34:21 +0100

    fix: mounting issue with singularity

--- openfoam_setup/run.sh
+++ openfoam_setup/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# SLURM job script for running OpenFOAM motorBike case on 8x c5n.4xlarge nodes
-
-#SBATCH --job-name=motorBike
+#SBATCH --job-name=motorBikeTutorial
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=16
 #SBATCH --partition=compute
 #SBATCH --output=/shared/cases/motorBikeTutorial/slurm-%j.log
 
 cd /shared/cases/motorBikeTutorial
-singularity exec /shared/containers/openfoam-run_2312.sif bash -c "./Allrun"
+singularity exec --bind /shared:/shared /shared/containers/openfoam-run_2312.sif bash -c "./Allrun"
