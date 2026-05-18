eb9bdb497fec7802a963eb2ae392acbf575518bf
NaclTea <53270339+NaclTea@users.noreply.github.com>
2026-05-18 10:17:52 +0100

    upate further mpi and envioprnment issues

--- openfoam_setup/run.sh
+++ openfoam_setup/run.sh
@@ -5,29 +5,43 @@
 #SBATCH --partition=compute
 #SBATCH --output=/shared/cases/motorBikeTutorial/slurm-%j.log
 
-# Create machinefile for mpirun
-scontrol show hostnames $SLURM_JOB_NODELIST > /tmp/machinefile
+# Get hostlist from SLURM
 HOSTS=$(scontrol show hostnames $SLURM_JOB_NODELIST | tr '\n' ',' | sed 's/,$//')
+
+# Set OpenFOAM source command
+OF_SOURCE="source /usr/lib/openfoam/openfoam2312/etc/bashrc"
 
 # Serial pre-processing
 singularity exec --bind /shared:/shared \
     /shared/containers/openfoam-run_2312.sif \
-    bash -c "source /usr/lib/openfoam/openfoam2312/etc/bashrc && \
-             cd /shared/cases/motorBikeTutorial && \
-             surfaceFeatures && blockMesh && decomposePar -copyZero"
+    bash --login -c "$OF_SOURCE && \
+                     cd /shared/cases/motorBikeTutorial && \
+                     surfaceFeatures && \
+                     blockMesh && \
+                     decomposePar -copyZero"
 
-# Parallel steps using container's own mpirun with explicit hosts
+# Parallel steps - force SSH transport, bypass SLURM launcher
 singularity exec --bind /shared:/shared \
     /shared/containers/openfoam-run_2312.sif \
-    bash -c "source /usr/lib/openfoam/openfoam2312/etc/bashrc && \
-             cd /shared/cases/motorBikeTutorial && \
-             mpirun -np 128 -H ${HOSTS} snappyHexMesh -overwrite -parallel && \
-             mpirun -np 128 -H ${HOSTS} potentialFoam -parallel && \
-             mpirun -np 128 -H ${HOSTS} simpleFoam -parallel"
+    bash --login -c "$OF_SOURCE && \
+                     cd /shared/cases/motorBikeTutorial && \
+                     mpirun --mca plm_rsh_agent ssh \
+                            --mca btl_tcp_if_include eth0 \
+                            -np 128 -H ${HOSTS} \
+                            snappyHexMesh -overwrite -parallel && \
+                     mpirun --mca plm_rsh_agent ssh \
+                            --mca btl_tcp_if_include eth0 \
+                            -np 128 -H ${HOSTS} \
+                            potentialFoam -parallel && \
+                     mpirun --mca plm_rsh_agent ssh \
+                            --mca btl_tcp_if_include eth0 \
+                            -np 128 -H ${HOSTS} \
+                            simpleFoam -parallel"
 
 # Reconstruct
 singularity exec --bind /shared:/shared \
     /shared/containers/openfoam-run_2312.sif \
-    bash -c "source /usr/lib/openfoam/openfoam2312/etc/bashrc && \
-             cd /shared/cases/motorBikeTutorial && \
-             reconstructParMesh -constant && reconstructPar -latestTime"
+    bash --login -c "$OF_SOURCE && \
+                     cd /shared/cases/motorBikeTutorial && \
+                     reconstructParMesh -constant && \
+                     reconstructPar -latestTime"
